5a9ec837d7c299a14179dd9785c11ec9c64e51c9
SAUTIERE <benoit.Sautiere@cellenza.com>
2019-08-15 10:52:25 +0200

    Adding demo script

--- AutomationAccount/DEMO.ps1
+++ AutomationAccount/DEMO.ps1
@@ -1,1 +1,10 @@
 Connect-AzAccount
+Get-Azsubscription
+Set-AzContext -subscriptionID 5be15500-7328-4beb-871a-1498cd4b4536
+$ResourceGroupName = "DemoPowerShellSaturday"
+$DeploymentName = "DEV" + (new-guid).guid 
+$TemplateFileURI = "https://raw.githubusercontent.com/Benoitsautierecellenza/PowerShellSaturday2019/master/AutomationAccount/AutomationAccount.JSON"
+$ParameterFileURI = "https://raw.githubusercontent.com/Benoitsautierecellenza/PowerShellSaturday2019/master/AutomationAccount/parameters.json"
+New-AzResourceGroup $ResourceGroupName -Location WestEurope
+New-AzResourceGroupDeployment -Name $DeploymentName -ResourceGroupName $ResourceGroupName -TemplateUri $TemplateFileURI -TemplateParameterUri $ParameterFileURI -automationAccountName LAB 
+
