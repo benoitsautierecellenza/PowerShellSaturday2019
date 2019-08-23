90c4e72901c1cd7bcc9d3456367f017d3ded74b1
SAUTIERE <benoit.Sautiere@cellenza.com>
2019-08-23 18:10:29 +0200

    Finalize SPN-GENERATOR.PS1

---
+++ AzureQueue/Formatmessages.ps1
@@ -0,0 +1,21 @@
+$PolicyParameterInfo = @(
+    @{
+        Name = 'ServiceName'                                            # Policy parameter name (mandatory)
+        DisplayName = 'windows service name.'                           # Policy parameter display name (mandatory)
+        Description = "Name of the windows service to be audited."      # Policy parameter description (optional)
+        ResourceType = "Service"                                        # DSC configuration resource type (mandatory)
+        ResourceId = 'windowsService'                                   # DSC configuration resource property name (mandatory)
+        ResourcePropertyName = "Name"                                   # DSC configuration resource property name (mandatory)
+        DefaultValue = 'winrm'                                          # Policy parameter default value (optional)
+        AllowedValues = @('BDESVC','TermService','wuauserv','winrm')    # Policy parameter allowed values (optional)
+    }
+)
+
+$MessageToQueue = @"
+{
+    "ServiceAzure" : "Storage/KeyVault, …",
+    "ActionType" : "CREATE/DELETE",
+    "resourceId" : "$resourceId",
+    "submissionTimestamp" : "$submissionTimestamp"
+}
+"@
